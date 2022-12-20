a5fc4c4bed4d6c80f250034f02f3b0a264907d09
sachin <sksachin7z2@gmail.com>
2022-12-20 21:48:03 +0530

    updated db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-const mongoURI="mongodb+srv://sksachin7z2:ramdulari123@cluster0.f01hh.mongodb.net/microtask2?authSource=admin&replicaSet=atlas-ppw553-shard-0&readPreference=primary&ssl=true"
+const mongoURI="mongodb+srv://[userName]:[password]@cluster0.f01hh.mongodb.net/microtask2?authSource=admin&replicaSet=atlas-ppw553-shard-0&readPreference=primary&ssl=true"
 
 const mongoconnect=()=>{
     mongoose.connect(mongoURI,()=>{
